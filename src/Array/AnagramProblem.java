82988ec0dca201343c7182f97a77d8690b2182cf
GeeKoders <iloveitone@gmail.com>
2020-07-09 22:09:47 +0800

    20200709 add AnagramProblem solution2

--- src/Array/AnagramProblem.java
+++ src/Array/AnagramProblem.java
@@ -8,9 +8,11 @@ public class AnagramProblem {
 
 		char[] firstWord = {'a', 'p', 'p', 'l', 'e'} ;
 		char[] secondWord = {'e', 'l', 'p', 'b', 'a'} ;
-		
+		String one = "Select" ;
+		String two = "leScet" ;
 		AnagramProblem anagramProblem = new AnagramProblem() ;
-		boolean result = anagramProblem.solution1(firstWord, secondWord) ;
+//		boolean result = anagramProblem.solution1(firstWord, secondWord) ;
+		boolean result = anagramProblem.solution2(one, two) ;
 		
 		System.out.println(result);
 	}
@@ -34,5 +36,29 @@ public class AnagramProblem {
 		return true ;
 		
 	}
+	
+	public boolean solution2(String one, String two){
+		
+		char[] firstWord = one.toLowerCase().toCharArray() ;
+		char[] secondWord = two.toLowerCase().toCharArray() ;
+		
+		if(firstWord.length != secondWord.length) return false ;
+			
+		//O(logN)
+		Arrays.sort(firstWord);
+		Arrays.sort(secondWord);
+		
+		//O(N)
+		for(int i=0; i<firstWord.length; i++){
+			
+			if(firstWord[i] != secondWord[i]){
+				return false ;
+			}
+		}
+		
+		return true ;
+		
+	}
+
 
 }
