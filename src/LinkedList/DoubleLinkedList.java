a385bd32aa8d8cbaf0268dcb835211f381c81a18
GeeKoders <iloveitone@gmail.com>
2020-07-16 20:59:04 +0800

    20200716 add double linkedlist insert head

---
+++ src/LinkedList/DoubleLinkedList.java
@@ -0,0 +1,69 @@
+package LinkedList;
+
+public class DoubleLinkedList<T> {
+
+	private DoubleNode<T> first ;
+	
+	private DoubleNode<T> last ;
+	
+	public static void main(String[] args) {
+
+		DoubleLinkedList doubleLinkedList = new DoubleLinkedList() ;
+		doubleLinkedList.insertH("1");
+		doubleLinkedList.insertH("2");
+		doubleLinkedList.insertH("3");
+		doubleLinkedList.insertH("4");
+		doubleLinkedList.insertH("5");
+		doubleLinkedList.print();
+		
+	}
+	
+	public void insertH(T data){
+		
+		DoubleNode<T> newNode = new DoubleNode<>(data) ;
+		
+		if(isEmpty()){
+			first = newNode ;
+			first.rNext = last ;
+			last = newNode ;
+			last.lNext = first ;
+		}else{
+			newNode.rNext = first ;
+			first.lNext = newNode ;
+			first = newNode ;
+		}
+	}
+
+	public boolean isEmpty(){
+		return first == null ;
+	}
+	
+	public void print(){
+		
+		DoubleNode<T> current = first ;
+		
+		while(current!=null){
+			System.out.println(current.data);
+			current = current.rNext ;
+		}
+		
+	}
+}
+
+
+
+class DoubleNode<T>{
+	
+	public T data ;
+	
+	public DoubleNode<T> lNext ;
+	
+	public DoubleNode<T> rNext ;
+	
+	public DoubleNode(T data) {
+		this.data = data ;
+		this.lNext = null ;
+		this.rNext = null ;
+	}
+	
+}
