258879739c84e3bb71f44753cee3a777c5e61eff
GeeKoders <iloveitone@gmail.com>
2020-07-26 12:04:29 +0800

    20200726 add Pascal triangle II solution2

--- src/recursion/PascalTriangleII.java
+++ src/recursion/PascalTriangleII.java
@@ -51,5 +51,16 @@ public class PascalTriangleII {
 			return f(row - 1, col - 1) + f(row - 1, col);
 		}
 	}
+	
+	public List<Integer> getRow2(int rowIndex) {
+	    List<Integer> res = new ArrayList<Integer>();
+	    for(int i = 0;i<rowIndex+1;i++) {
+	    		res.add(1);
+	    		for(int j=i-1;j>0;j--) {
+	    			res.set(j, res.get(j-1)+res.get(j));
+	    		}
+	    }
+	    return res;
+	}
 
 }
