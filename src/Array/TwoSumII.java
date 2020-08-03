fe6c73058ec009b57bc4ed90b7a7b9e20693842e
GeeKoders <iloveitone@gmail.com>
2020-08-03 22:03:04 +0800

    20200803 add Two sum solution2

--- src/Array/TwoSumII.java
+++ src/Array/TwoSumII.java
@@ -52,5 +52,24 @@ public class TwoSumII {
         
         return twoSum;
     }
+	
+	//the best solution
+	public int[] twoSum2(int[] numbers, int target) {
+        
+        int start = 0 ; 
+        int end = numbers.length - 1 ;
+        
+        while(start < end){
+            
+            if( (numbers[start] + numbers[end]) == target ) break ;
+            if( (numbers[start] + numbers[end]) < target ){
+                start ++ ;
+            }else{
+                end -- ;
+            }
+        }
+        
+        return new int[]{start+1, end+1} ; 
+    }
 
 }
