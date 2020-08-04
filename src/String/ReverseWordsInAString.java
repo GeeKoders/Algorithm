553196fadc22232f26145a5d143ee0b196034c62
GeeKoders <iloveitone@gmail.com>
2020-08-04 22:26:17 +0800

    20200804 add reverse words in a string

---
+++ src/String/ReverseWordsInAString.java
@@ -0,0 +1,39 @@
+package String;
+
+public class ReverseWordsInAString {
+
+	public static void main(String[] args) {
+		ReverseWordsInAString reverseWordsInAString = new ReverseWordsInAString() ;
+		String result = reverseWordsInAString.reverseWords("a good   example") ;
+		System.out.println("result:" + result);
+	}
+	// wrong answer
+	public String reverseWords(String s) {
+        
+        String data = s.trim() ;
+        String [] split = data.split(" ") ;
+        
+        int start = 0 ;
+        int end = split.length - 1 ;
+        
+        while(start < end){
+            swap(split, start, end) ;
+            start++;
+            end--;
+        }
+        
+        String result = "" ;
+        for(String item: split){
+            result += item.trim() + " " ;
+        }
+        
+        return result.trim() ;
+    }
+    
+    public void swap(String []split, int i, int j){
+        String tmp = split[i].trim() ;
+        split[i] = split[j].trim() ;
+        split[j] = tmp ;
+    }
+
+}
