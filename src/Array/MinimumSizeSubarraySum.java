55a38490c7de156c1a63c6629c10ce3e99499518
GeeKoders <iloveitone@gmail.com>
2020-08-04 08:16:10 +0800

    20200804 add Minimum size subarray sum description

--- src/Array/MinimumSizeSubarraySum.java
+++ src/Array/MinimumSizeSubarraySum.java
@@ -2,6 +2,24 @@ package Array;
 
 public class MinimumSizeSubarraySum {
 
+	
+	/*
+	 * https://leetcode.com/explore/learn/card/array-and-string/205/array-two-pointer-technique/1299/
+	 * 
+	 * Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of
+	 * which the sum ≥ s. If there isn't one, return 0 instead.
+	 * 
+	 * Example: 
+	 * Input: s = 7, nums = [2,3,1,2,4,3]
+	 * Output: 2
+	 * Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+	 * 
+	 * Follow up:
+	 * If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n). 
+	 *
+	 * 
+	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
