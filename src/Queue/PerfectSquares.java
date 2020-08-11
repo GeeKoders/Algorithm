9a4479d815d550ea5fecd5e230b7305b535a7000
GeeKoders <iloveitone@gmail.com>
2020-08-11 20:59:22 +0800

    20200811 add perfect squares solution2

--- src/Queue/PerfectSquares.java
+++ src/Queue/PerfectSquares.java
@@ -1,6 +1,10 @@
 package Queue;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 
 public class PerfectSquares {
 
@@ -56,4 +60,41 @@ public class PerfectSquares {
         return dp[n] ;            
         
     }
+    
+    //BFS
+    public int numSquares2(int n) {
+    	
+    	Queue<Integer> queue = new LinkedList<>() ;
+    	Set<Integer> visited = new HashSet<>() ;
+    	queue.offer(0) ;
+    	visited.add(0) ;
+    	int depth = 0 ;
+    	while(!queue.isEmpty()){
+    		int size = queue.size();
+    		depth++ ;
+    		while(size-->0){
+    			int u = queue.poll() ;
+    			for(int i=1; i*i <=n; i++){
+    				int v = u + i*i ;
+    				
+    				if(v == n) return depth ;
+    				
+    				if(v > n) break ;
+    				
+    				if(!visited.contains(v)){
+    					queue.offer(v) ;
+    					visited.add(v) ;
+    				}
+    				
+    			}
+    			
+    		}
+    		
+    		
+    	}
+    	return depth ;
+    }
+    
+    
+    
 }
