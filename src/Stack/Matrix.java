336d837f647a74d2be2bd938d91b3e906d879a3f
GeeKoders <iloveitone@gmail.com>
2020-08-19 08:33:28 +0800

    20200819 add Matrix description

--- src/Stack/Matrix.java
+++ src/Stack/Matrix.java
@@ -5,6 +5,43 @@ import java.util.Queue;
 
 public class Matrix {
 
+	
+	
+	/*
+	 * https://leetcode.com/explore/learn/card/queue-stack/239/conclusion/1388/
+	 * solution: https://leetcode.com/problems/01-matrix/solution/
+	 * 
+	 * Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
+	 * The distance between two adjacent cells is 1.
+	 * 
+	 * Example 1:
+	 * Input:
+	 * [[0,0,0],
+	 *  [0,1,0],
+	 *  [0,0,0]]
+	 *  
+	 * Output: 
+	 * [[0,0,0],
+	 * [0,1,0], 
+	 * [0,0,0]]
+	 * 
+	 * Example 2:
+	 * Input:
+	 * [[0,0,0],
+	 * [0,1,0],
+	 * [1,1,1]]
+	 * 
+	 * Output:
+	 * [[0,0,0],
+	 * [0,1,0],
+	 * [1,2,1]]
+	 * 
+	 * Note:
+	 * 1.The number of elements of the given matrix will not exceed 10,000.
+	 * 2.There are at least one 0 in the given matrix.
+	 * 3.he cells are adjacent in only four directions: up, down, left and right.
+	 * 
+	 */
 	public static void main(String[] args) {
 
 		
