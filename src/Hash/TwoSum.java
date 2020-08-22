4e728e091c279b103e691a190cb88c6191776ff3
GeeKoders <iloveitone@gmail.com>
2020-08-22 11:30:49 +0800

    20200822 add Twosum solution 4

--- src/Hash/TwoSum.java
+++ src/Hash/TwoSum.java
@@ -83,5 +83,18 @@ public class TwoSum {
         
         return new int[]{} ;
     }
+	//O(N), O(N)
+	public int[] twoSum4(int[] nums, int target) {
+	    Map<Integer, Integer> map = new HashMap<>();
+	    for (int i = 0; i < nums.length; i++) {
+	        int complement = target - nums[i];
+	        if (map.containsKey(complement)) {
+	            return new int[] { map.get(complement), i };
+	        }
+	        map.put(nums[i], i);
+	    }
+	    
+	    return new int[]{} ;
+	}
 
 }
