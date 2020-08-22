0f78153ec15312f3587ac422efdeff16ed1641f2
GeeKoders <iloveitone@gmail.com>
2020-08-22 11:32:13 +0800

    20200822 add Twosum solution5

--- src/Hash/TwoSum.java
+++ src/Hash/TwoSum.java
@@ -96,5 +96,22 @@ public class TwoSum {
 	    
 	    return new int[]{} ;
 	}
+	
+	public int[] twoSum5(int[] nums, int target) {
+        Map <Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                res[0] = i;
+                res[1] = map.get(target - nums[i]);
+                return res;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        
+        return res;
+    }
 
 }
