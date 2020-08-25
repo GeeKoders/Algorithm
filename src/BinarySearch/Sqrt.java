aa7f8328f14f191984837524845683d43ade10c2
GeeKoders <iloveitone@gmail.com>
2020-08-25 22:17:14 +0800

    20200825 add Sqrt solution2

--- src/BinarySearch/Sqrt.java
+++ src/BinarySearch/Sqrt.java
@@ -29,6 +29,30 @@ public class Sqrt {
 	public int mySqrt(int x) {
         return (int)Math.sqrt(x) ;
     }
+	//reference: https://www.youtube.com/watch?v=3MyA0dj-_2c
+	public int mySqrt2(int x) {
+		
+		long start = 0 ;
+		long end = x ;
+		
+		while(start + 1 < end){
+			
+			long mid = start + (end - start) / 2 ;
+			
+			if(mid * mid == x) return (int)mid ;
+			
+			if(mid * mid < x){
+				start = mid ;
+			}else{
+				end = mid ;
+			}
+			
+		}
+		
+		if(end * end == x) return (int) end ;
+		return (int) start ;
+		
+    }
 	
 	
 }
