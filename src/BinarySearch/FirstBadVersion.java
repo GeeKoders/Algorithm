fa8b174d30a362601247ca4935b9cc6e854365c7
GeeKoders <iloveitone@gmail.com>
2020-08-26 23:07:02 +0800

    20200826 add First Bad Version solution2

--- src/BinarySearch/FirstBadVersion.java
+++ src/BinarySearch/FirstBadVersion.java
@@ -42,6 +42,28 @@ public class FirstBadVersion {
 	    return left;
 	}
 	
+	public int firstBadVersion2(int n) {
+        
+        if(n < 1) return -1 ;
+       
+            
+        int left = 1 ;
+        int right = n ;
+        while(left <= right){
+            
+            int mid = left + (right - left) / 2 ;
+            
+            if(isBadVersion(mid)){
+                right = mid - 1  ;
+            }else{
+                left = mid + 1 ;
+            }
+        }
+        
+
+        return left ;
+    }
+	
 	/* The isBadVersion API is defined in the parent class VersionControl.
     boolean isBadVersion(int version); */
 
