86e0f7f0cca4c64a5fb981ed0a2d5a403b0988de
GeeKoders <iloveitone@gmail.com>
2020-08-26 20:35:46 +0800

    20200826 add Guess Number Higher or Lower

---
+++ src/BinarySearch/GuessNumberHigherorLower.java
@@ -0,0 +1,47 @@
+package BinarySearch;
+
+public class GuessNumberHigherorLower {
+
+	
+	/*
+	 * https://leetcode.com/explore/learn/card/binary-search/125/template-i/951/
+	 * solution: https://leetcode.com/problems/guess-number-higher-or-lower/solution/
+	 * 
+	 * We are playing the Guess Game. The game is as follows:
+	 * I pick a number from 1 to n. You have to guess which number I picked.
+	 * Every time you guess wrong, I'll tell you whether the number is higher or lower.
+	 * You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+	 * 
+	 * -1 : My number is lower
+	 *  1 : My number is higher
+	 *  0 : Congrats! You got it!
+	 *  
+	 *  Example :
+	 *  Input: n = 10, pick = 6
+	 *  Output: 6
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	//O(N), O(1)
+	public int guessNumber(int n) {
+        for (int i = 1; i < n; i++)
+            if (guess(i) == 0)
+                return i;
+        return n;
+    }
+	
+	
+	
+	
+	/** 
+	 * Forward declaration of guess API.
+	 * @param  num   your guess
+	 * @return 	     -1 if num is lower than the guess number
+	 *			      1 if num is higher than the guess number
+	 *               otherwise return 0
+	 * int guess(int num);
+	 */
+
+}
