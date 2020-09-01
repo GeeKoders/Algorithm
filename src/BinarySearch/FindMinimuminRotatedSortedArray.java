f84a1d41e5cfeecd627e1fc11e5db66c8e2fea8b
GeeKoders <iloveitone@gmail.com>
2020-09-01 08:17:04 +0800

    20200901 add Find Minimum in Rotated Sorted Array  Solution solution2

--- src/BinarySearch/FindMinimuminRotatedSortedArray.java
+++ src/BinarySearch/FindMinimuminRotatedSortedArray.java
@@ -49,7 +49,20 @@ public class FindMinimuminRotatedSortedArray {
        }
        return nums[start];
        
-       
-       
    }
+
+	public int findMin2(int[] nums) {
+        
+        int lo = 0, hi = nums.length-1;
+        while (lo < hi) {
+            int mid = lo + (hi-lo)/2;
+            if (nums[mid] < nums[hi]) {
+                hi = mid;
+            } else {
+                lo = mid+1;
+            }
+        }
+        return nums[lo];
+    }
+	
 }
