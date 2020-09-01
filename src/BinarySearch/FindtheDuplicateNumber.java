9746d912412c1d01a12e5e96627d7f1291885dd6
GeeKoders <iloveitone@gmail.com>
2020-09-01 22:55:16 +0800

    20200901 add Find the Duplicate Number solution5

--- src/BinarySearch/FindtheDuplicateNumber.java
+++ src/BinarySearch/FindtheDuplicateNumber.java
@@ -117,5 +117,20 @@ public class FindtheDuplicateNumber {
         }
     }
 	
+	public int findDuplicate5(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+        slow = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+	
 
 }
