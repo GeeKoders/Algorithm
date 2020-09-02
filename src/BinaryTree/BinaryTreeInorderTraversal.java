d6f275bac92b23360528d4e2c785fa3b7a1977c5
GeeKoders <iloveitone@gmail.com>
2020-09-02 22:38:47 +0800

    20200902 fix Binary tree inorder traversal

--- src/BinaryTree/BinaryTreeInorderTraversal.java
+++ src/BinaryTree/BinaryTreeInorderTraversal.java
@@ -82,18 +82,6 @@ public class BinaryTreeInorderTraversal {
         
     }
 	
-    class TreeNode {
-    	int val;
-    	TreeNode left;
-    	TreeNode right;
-    	TreeNode() {}
-    	TreeNode(int val) { this.val = val; }
-    	TreeNode(int val, TreeNode left, TreeNode right) {
-    		this.val = val;
-    		this.left = left;
-    		this.right = right;
-    	}
-    }
 }
 
 class TreeNode {
