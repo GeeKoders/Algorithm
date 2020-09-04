13f63c777e314f7fc65770f473df34c4cf9bf3bd
GeeKoders <iloveitone@gmail.com>
2020-09-04 08:29:52 +0800

    20200904 fix pre order traverse

--- src/BinaryTree/PreOrderTraverse.java
+++ src/BinaryTree/PreOrderTraverse.java
@@ -2,6 +2,7 @@ package BinaryTree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class PreOrderTraverse {
 
@@ -11,19 +12,19 @@ public class PreOrderTraverse {
 
 	}
 
-	public List<Integer> postorderTraversal(TreeNode root) {
+	public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         if(root == null) return list;
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
         while(!stack.isEmpty()) {
-            TreeNode curr = stack.pop();
-            list.add(0,curr.val);
-            if(curr.left!=null) {
-              stack.push(curr.left);
+            TreeNode current = stack.pop();
+            list.add(current.val);
+            if(current.right!=null) {
+               stack.push(current.right);
             }
-            if(curr.right!=null) {
-               stack.push(curr.right); 
+            if(current.left!=null) {
+              stack.push(current.left);
             }
         }
         return list;
