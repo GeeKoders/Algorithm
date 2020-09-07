cbef6bcdc500ab057332e82bb0f7d3674f2f2198
GeeKoders <iloveitone@gmail.com>
2020-09-07 20:31:11 +0800

    20200907 add Populating Next Right Pointers in Each Node solution2

--- src/BinarySearch/PopulatingNextRightPointersinEachNode.java
+++ src/BinarySearch/PopulatingNextRightPointersinEachNode.java
@@ -47,10 +47,35 @@ public class PopulatingNextRightPointersinEachNode {
 				}
 				cur = cur.next;
 			}
+			
 		}
 		root = root.left;
 
 		return root;
+	}
+	
+	public Node connect2(Node root) {
+        
+	      if(root == null) return root;
+	        
+	        Node curr = root;
+	        while(curr != null) {
+	            Node head = curr;
+	            while(curr != null) {
+	               if(curr.left != null) {
+	                   curr.left.next = curr.right;
+	               }
+	                
+	                if(curr.right != null && curr.next != null) {
+	                    curr.right.next = curr.next.left;
+	                }
+	                curr = curr.next;
+	            }
+	            curr = head.left;
+	        }
+	        
+	        return root;
+	        
 	}
 
 }
