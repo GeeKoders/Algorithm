c2cc35efb6e713b7912e680e674ecce599a4ec09
GeeKoders <iloveitone@gmail.com>
2020-12-22 08:14:11 +0800

    20201222 add BinarySearchTreeIterator

---
+++ src/BinarySearchTree/BinarySearchTreeIterator.java
@@ -0,0 +1,73 @@
+package BinarySearchTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinarySearchTreeIterator {
+
+	class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	
+	/*
+	 * https://leetcode.com/explore/learn/card/introduction-to-data-structure-binary-search-tree/140/introduction-to-a-bst/1008/
+	 * 
+	 * solution: https://leetcode.com/problems/binary-search-tree-iterator/solution/
+	 *
+	 */
+	
+	public static void main(String[] args) {
+//		BinarySearchTreeIterator obj = new BinarySearchTreeIterator(root);
+//		int param_1 = obj.next();
+//		boolean param_2 = obj.hasNext();
+	}
+	
+	private List<Integer> list ;
+    
+    private int index ;
+ 
+    public BinarySearchTreeIterator(TreeNode root) {
+        
+        list = new ArrayList<>() ;
+        
+        index = -1 ;
+        
+        inorder(root) ;        
+        
+    }
+    
+    private void inorder(TreeNode root){
+        
+        if (root == null) return ;
+        
+        inorder(root.left) ;
+        list.add(root.val) ;
+        inorder(root.right) ;
+        
+        
+    }
+    
+    public int next() {
+        return list.get(++index) ;
+    }
+    
+    public boolean hasNext() {
+        return index+1 < list.size() ;
+    }
+
+}
