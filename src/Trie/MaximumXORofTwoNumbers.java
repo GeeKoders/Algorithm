5701e02dd6eb34d8ca149d4cad00f8a13fd12f1b
GeeKoders <iloveitone@gmail.com>
2021-01-09 20:10:08 +0800

    20210109 add Maximum xor of two numbers in an array solution2

--- src/Trie/MaximumXORofTwoNumbers.java
+++ src/Trie/MaximumXORofTwoNumbers.java
@@ -42,5 +42,27 @@ public class MaximumXORofTwoNumbers {
 	    }
 	    return maxXor;
 	  }
+	
+	// approach2
+	/*
+	 * 
+	 * 
+	 * Time Limit Exceeded
+	 */
+	
+	public int findMaximumXOR2(int[] nums) {
+		
+		int max = 0 ;
+		for(int i = 0; i< nums.length-1; i++){
+			for(int j = i+1 ; j<nums.length  ; j++){
+				max = Math.max(max, nums[i] ^ nums[j]) ;
+			}
+			
+			
+		}
+		
+		return max ;
+		
+	}
 		
 }
