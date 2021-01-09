35f793151650fe0759c7fec0754d0c56c748d1a9
GeeKoders <iloveitone@gmail.com>
2021-01-09 20:33:45 +0800

    20210109 add Maximum xor of two numbers in an array solution3

--- src/Trie/MaximumXORofTwoNumbers.java
+++ src/Trie/MaximumXORofTwoNumbers.java
@@ -45,7 +45,7 @@ public class MaximumXORofTwoNumbers {
 	
 	// approach2
 	/*
-	 * 
+	 * https://www.youtube.com/watch?v=HAuqbJojVPk&t=2s&ab_channel=NareshGupta
 	 * 
 	 * Time Limit Exceeded
 	 */
@@ -62,6 +62,50 @@ public class MaximumXORofTwoNumbers {
 		}
 		
 		return max ;
+		
+	}
+	
+	/*
+	 * approach3
+	 * solution: https://www.youtube.com/watch?v=blsm1fA9JHQ&ab_channel=coderZ
+	 * 
+	 * [3, 10, 5, 25, 2, 8] = [00011, 01010, 00101, 11001, 00010, 01000]
+	 * 
+	 * a^b=c, a^c=b
+	 * a^b=max, a^max=b
+	 * mask = 10000 -> maximum = 10000 ? true
+	 * mask = 11000 -> maximum = 11000 ? true
+	 * mask = 11100 -> maximum = 11100 ? true
+	 * mask = 11110 -> maximum = 11110 ? false -> maximum = 11100
+	 * mask = 11111 -> maximum = 11101 ? false -> maximum = 11100 > 28
+	 * 
+	 * 
+		39 / 39 test cases passed.
+		Status: Accepted
+		Runtime: 76 ms (Your runtime beats 46.93 % of java submissions.)
+		Memory Usage: 40 MB
+	 * 
+	 */
+	public int findMaximumXOR3(int [] nums){
+		
+		int maximum = 0 ; 
+		int mask = 0 ;
+		for(int i = 30; i>=0; i--){
+			int one = 1 << i ;
+			mask = mask | one ;
+			HashSet<Integer> set = new HashSet<>() ;
+			for(int num: nums) set.add(mask & num) ;
+			for(int val: set){
+				if(set.contains(val ^ (maximum | one))){
+					maximum = maximum | one ;
+					break ;
+				}
+			}
+			
+			
+		}
+		return maximum ;
+		
 		
 	}
 		
