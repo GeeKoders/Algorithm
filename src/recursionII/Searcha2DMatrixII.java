68c367d2d253318238d4f0fd0c4f45fb5004a040
GeeKoders <iloveitone@gmail.com>
2021-01-13 05:33:13 +0800

    20210113 add Search a 2D matrix with binary search (It must fix because of the bug)

--- src/recursionII/Searcha2DMatrixII.java
+++ src/recursionII/Searcha2DMatrixII.java
@@ -64,5 +64,80 @@ public class Searcha2DMatrixII {
 		
 		
 	}
+	// solution3
+	private int[][] matrix;
+    private int target;
+    
+    public boolean searchMatrix3(int[][] mat, int targ) {
+        // cache input values in object to avoid passing them unnecessarily
+        // to `searchRec`
+        matrix = mat;
+        target = targ;
 
+        // an empty matrix obviously does not contain `target`
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+
+        return searchRec(0, 0, matrix[0].length-1, matrix.length-1);
+    }
+
+    private boolean searchRec(int left, int up, int right, int down) {
+        // this submatrix has no height or no width.
+        if (left > right || up > down) {
+            return false;
+        // `target` is already larger than the largest element or smaller
+        // than the smallest element in this submatrix.
+        } else if (target < matrix[up][left] || target > matrix[down][right]) {
+            return false;
+        }
+
+        int mid = left + (right-left)/2;
+
+        // Locate `row` such that matrix[row-1][mid] < target < matrix[row][mid]
+        int row = up;
+        while (row <= down && matrix[row][mid] <= target) {
+            if (matrix[row][mid] == target) {
+                return true;
+            }
+            row++;
+        }
+
+        //go to bottom-left or top-right
+        return searchRec(left, row, mid-1, down) || searchRec(mid+1, up, right, row-1);
+    }
+    
+    /*
+     * binary search
+     * 
+     * soltuion: https://www.youtube.com/watch?v=eT0UqrYuqbg&ab_channel=NickWhite
+     *
+     */
+    public boolean searchMatrix4(int [][] matrix, int target){
+
+    	if(matrix == null || matrix.length == 0) return false ;
+    	
+    	int rows = matrix.length ;
+    	int columns = matrix[0].length ;
+    	
+    	int left = 0 ;
+    	int right = rows * columns - 1 ;
+    	
+    	while(left <= right){
+    		
+    		int mid = left + (right - left) / 2 ;
+    		int mid_element = matrix[mid/columns][mid*columns] ;
+    		if(target == mid_element){
+    			return true ;
+    		}else if(target < mid_element){
+    			right = mid - 1 ;
+    		}else if(target > mid_element){
+    			left = mid + 1 ;
+    		}
+    	}
+    	
+    	return false ;
+    	
+    	
+    }
 }
