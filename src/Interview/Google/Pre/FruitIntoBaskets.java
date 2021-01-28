1fac121db4152e02a5693c4e2d49b557a3c11bb3
GeeKoders <iloveitone@gmail.com>
2021-01-29 07:04:28 +0800

    20210129 add Fruit into baskets

---
+++ src/Interview/Google/Pre/FruitIntoBaskets.java
@@ -0,0 +1,60 @@
+package Interview.Google.Pre;
+
+public class FruitIntoBaskets {
+
+	/*
+	 * 904. Fruit Into Baskets (Medium)
+	 * 
+	 * https://leetcode.com/problems/fruit-into-baskets/
+	 * 
+	 * https://leetcode.com/problems/fruit-into-baskets/solution/
+	 * 
+	 * reference: 
+	 * 
+	 * https://www.youtube.com/watch?v=s_zu2dOkq80&ab_channel=NickWhite
+	 * 
+	 * https://www.youtube.com/watch?v=za2YuucS0tw&ab_channel=KevinNaughtonJr.
+	 * 
+	 * Runtime: 18 ms, faster than 72.28% of Java online submissions for Fruit Into Baskets.
+	 * 
+	 * Memory Usage: 85 MB, less than 10.23% of Java online submissions for Fruit Into Baskets.
+	 * 
+	 * Time complexity: O(N) 
+	 * Space complexity: O(1)
+	 * 
+	 * sliding window
+	 */
+	public int totalFruit(int[] tree) {
+
+		int last_fruit = -1;
+		int second_last_fruit = -1;
+		int last_fruit_count = 0;
+		int current_max = 0;
+		int max = 0;
+
+		for (Integer fruit : tree) {
+
+			if (fruit == last_fruit || fruit == second_last_fruit) {
+				current_max += 1;
+			} else {
+				current_max = last_fruit_count + 1;
+			}
+
+			if (fruit == last_fruit) {
+				last_fruit_count++;
+			} else {
+				last_fruit_count = 1;
+			}
+			// update
+			if (fruit != last_fruit) {
+				second_last_fruit = last_fruit;
+				last_fruit = fruit;
+			}
+
+			max = Math.max(current_max, max);
+		}
+
+		return max;
+
+	}
+}
