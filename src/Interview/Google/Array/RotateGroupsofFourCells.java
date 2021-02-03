88c9e61ddfce20e68eef783d20d5f61ec6e60b08
GeeKoders <iloveitone@gmail.com>
2021-02-03 08:24:54 +0800

    20210203 add Rotate groups of four cells solution2

--- src/Interview/Google/Array/RotateGroupsofFourCells.java
+++ src/Interview/Google/Array/RotateGroupsofFourCells.java
@@ -29,4 +29,41 @@ public class RotateGroupsofFourCells {
 			}
 		}
 	}
+	
+	/*
+	 * discuss: https://leetcode.com/problems/rotate-image/discuss/18872/A-common-method-to-rotate-the-image
+	 * 
+	 * clockwise rotate
+	 * first reverse up to down, then swap the symmetry 
+	 * 1 2 3     7 8 9     7 4 1
+	 * 4 5 6  => 4 5 6  => 8 5 2
+	 * 7 8 9     1 2 3     9 6 3
+	 * 
+	 * Runtime: 0 ms, faster than 100.00% of Java online submissions for Rotate Image.
+	 * Memory Usage: 39.4 MB, less than 15.71% of Java online submissions for Rotate Image.
+	 * 
+	 */
+	public void rotate2(int[][] matrix) {
+	
+		int s=0 ; 
+		int e = matrix.length - 1 ;
+		while(s < e){
+			
+			int[] temp = matrix[s] ;
+			matrix[s] = matrix[e] ;
+			matrix[e] = temp ;
+			s++; 
+			e-- ;
+		}
+		
+		for(int i=0; i<matrix.length; i++){
+			for(int j=i+1; j<matrix[i].length; j++){
+				int temp = matrix[i][j] ;
+				matrix[i][j] = matrix[j][i] ;
+				matrix[j][i] = temp ;
+			}
+		}
+		
+	}
+	
 }
