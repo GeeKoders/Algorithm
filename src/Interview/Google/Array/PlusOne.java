b42ed757311425caf6cd558e3fecdb8211d70bea
GeeKoders <iloveitone@gmail.com>
2021-02-05 07:13:01 +0800

    20210205 add Plus one solution2

--- src/Interview/Google/Array/PlusOne.java
+++ src/Interview/Google/Array/PlusOne.java
@@ -1,8 +1,7 @@
 package Interview.Google.Array;
 
 public class PlusOne {
-	
-	
+
 	/*
 	 * 66. Plus One (Easy)
 	 * 
@@ -10,13 +9,12 @@ public class PlusOne {
 	 * 
 	 * solution: https://leetcode.com/problems/plus-one/solution/
 	 * 
-	 * Runtime: 0 ms, faster than 100.00% of Java online submissions for Plus One.
+	 * Runtime: 0 ms, faster than 100.00% of Java online submissions for Plus One. 
+	 * 
 	 * Memory Usage: 38.9 MB, less than 12.23% of Java online submissions for Plus One.
 	 * 
-	 * 
-	 * Time complexity: O(N)
+	 * Time complexity: O(N) 
 	 * Space complexity: O(N)
-	 * 
 	 */
 	public int[] plusOne(int[] digits) {
 
@@ -35,4 +33,34 @@ public class PlusOne {
 
 		return res;
 	}
+
+	public int[] plusOne2(int[] digits) {
+
+		int[] res = new int[digits.length + 1];
+
+		for (int i = digits.length - 1; i >= 0; i--) {
+
+			int sum = digits[i] + 1;
+			int divider = sum / 10;
+
+			if (divider == 1) {
+				digits[i] = 0;
+			} else {
+				digits[i]++;
+				return digits;
+			}
+
+		}
+
+		for (int i = 0; i < res.length; i++) {
+			if (i == 0) {
+				res[i] = 1;
+			} else {
+				res[i] = digits[i - 1];
+			}
+		}
+
+		return res;
+	}
+
 }
