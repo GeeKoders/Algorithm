db9900f60af938b8ec31ed74e1e03d2f2349e4ae
GeeKoders <iloveitone@gmail.com>
2021-02-07 09:47:50 +0800

    20210207 Minimum windows substring solution 2

--- src/Interview/Google/Array/MinimumWindowsSubstring.java
+++ src/Interview/Google/Array/MinimumWindowsSubstring.java
@@ -89,5 +89,54 @@ public class MinimumWindowsSubstring {
 		return minLength == Integer.MAX_VALUE ? "" : s.substring(minStart,
 				minEnd);
 	}
+	
+	
+	/*
+	 * reference:
+	 * 
+	 * https://www.youtube.com/watch?v=vvVe7qxOz9U&ab_channel=happygirlzt
+     *
+     * Your runtime beats 84.75 % of java submissions.
+     *
+     * Your memory usage beats 57.81 % of java submissions.
+     * 
+	 */
+	public String minWindow2(String s, String t) {
+	
+		if (s == null || s.length() == 0) return "";
+        int lo = 0, hi = 0;
+        int minLen = Integer.MAX_VALUE;
+        int minStart = -1, minEnd = -1;
 
+        int[] map = new int[128];
+        for (char c : t.toCharArray()) {
+            map[c]++;
+        }
+
+        int count = 0;
+        for (hi = 0; hi < s.length(); hi++) {
+            if (map[s.charAt(hi)] > 0) {
+                count++;
+            }
+            map[s.charAt(hi)]--;
+            if (count == t.length()) {
+                while (lo < hi && map[s.charAt(lo)] < 0) {
+                    map[s.charAt(lo)]++;
+                    lo++;
+                }
+
+                if (hi - lo + 1 < minLen) {
+                    minLen = hi - lo + 1;
+                    minStart = lo;
+                    minEnd = hi + 1;
+                }
+                map[s.charAt(lo)]++;
+                count--;
+                lo++;
+            }
+        }
+
+        return (minStart == -1) ? "" : s.substring(minStart, minEnd);
+		
+	}
 }
