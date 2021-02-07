33f6fd9c5de42803eaa3bc962772d3a85dfd286f
GeeKoders <iloveitone@gmail.com>
2021-02-07 14:41:46 +0800

    20210207 add Longest Substring with At Most Two Distinct Characters solution2

--- src/Interview/Google/Array/LongestSubstringwithAtMostTwoDistinctCharacters.java
+++ src/Interview/Google/Array/LongestSubstringwithAtMostTwoDistinctCharacters.java
@@ -1,5 +1,6 @@
 package Interview.Google.Array;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,7 +10,9 @@ public class LongestSubstringwithAtMostTwoDistinctCharacters {
 	/*
 	 * 159. Longest Substring with At Most Two Distinct Characters (Mdeium)
 	 * 
-	 * https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/solution/
+	 * https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/
+	 * 
+	 * solution: https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/solution/
 	 * 
 	 * Runtime: 9 ms, faster than 43.53% of Java online submissions for Longest Substring with At Most Two Distinct Characters.
 	 * 
@@ -61,6 +64,43 @@ public class LongestSubstringwithAtMostTwoDistinctCharacters {
 
 		return smallestIndex;
 
+	}
+	/*
+	 * 
+	 * video solution
+	 */
+	public int lengthOfLongestSubstringTwoDistinct2(String s) {
+		
+		 int n = s.length();
+		    if (n < 3) return n;
+
+		    // sliding window left and right pointers
+		    int left = 0;
+		    int right = 0;
+		    // hashmap character -> its rightmost position
+		    // in the sliding window
+		    HashMap<Character, Integer> hashmap = new HashMap<Character, Integer>();
+
+		    int max_len = 2;
+
+		    while (right < n) {
+		      // when the slidewindow contains less than 3 characters
+		      hashmap.put(s.charAt(right), right++);
+
+		      // slidewindow contains 3 characters
+		      if (hashmap.size() == 3) {
+		        // delete the leftmost character
+		        int del_idx = Collections.min(hashmap.values());
+		        hashmap.remove(s.charAt(del_idx));
+		        // move left pointer of the slidewindow
+		        left = del_idx + 1;
+		      }
+
+		      max_len = Math.max(max_len, right - left);
+		    }
+		    return max_len;
+		
+		
 	}
 
 }
