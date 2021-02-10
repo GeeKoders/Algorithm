0b7b1924ddbd902a858882c0406b51e3124e27c0
GeeKoders <iloveitone@gmail.com>
2021-02-10 16:11:07 +0800

    20210210 add Pascal's Triangle

--- src/Learn/Array/PascalTriangle.java
+++ src/Learn/Array/PascalTriangle.java
@@ -1,4 +1,4 @@
-package Array;
+package Learn.Array;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,10 +8,18 @@ public class PascalTriangle {
 	
 	
 	/*
+	 * 118. Pascal's Triangle (Easy)
+	 * 
 	 * https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1170/
 	 * solution: https://leetcode.com/articles/pascals-triangle/
 	 * 
 	 * Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+	 * 
+	 * Runtime: 0 ms, faster than 100.00% of Java online submissions for Pascal's Triangle.
+	 * Memory Usage: 36.6 MB, less than 94.85% of Java online submissions for Pascal's Triangle.
+	 * 
+	 * Time complexity: O(numRows^2)
+	 * Space complexity: O(numRows^2)
 	 * 
 	 */
 	public static void main(String[] args) {
