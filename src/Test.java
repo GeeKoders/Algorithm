05ccdca824d6708ced0c4222e21c920f6820e9a7
GeeKoders <iloveitone@gmail.com>
2021-02-11 10:48:15 +0800

    20210211 find Kth largest element in an Array and k = 2

--- src/Test.java
+++ src/Test.java
@@ -8,14 +8,18 @@ import javafx.util.Pair;
 public class Test {
 
 	public static void main(String[] args) {
-//		Test test = new Test() ;
+		Test test = new Test() ;
 //		test.mapLoop();
 		
-		Pair<Integer, String> pair = new Pair<>(1, "One");
-		Integer key = pair.getKey();
-	    String value = pair.getValue();
-	    System.out.println("key:" + key);
-	    System.out.println("value:" + value);
+//		Pair<Integer, String> pair = new Pair<>(1, "One");
+//		Integer key = pair.getKey();
+//	    String value = pair.getValue();
+//	    System.out.println("key:" + key);
+//	    System.out.println("value:" + value);
+		
+		int[] nums = {5,6,7,8,9,10} ;
+		
+		System.out.println(test.findKLarget(nums));
 		
 	}
 	
@@ -44,6 +48,38 @@ public class Test {
 		        Map.Entry pairs = (Map.Entry)it.next();   
 		        System.out.println(pairs.getKey() + "=" + pairs.getValue());   
 		 } 
+	}
+	
+	private int findKLarget(int[] nums){
+		
+		
+		Integer first = null ;
+		Integer second = null ;
+		
+		for(int i=0; i<nums.length - 1; i++){
+			
+			int tmp = nums[i] ;
+			
+			if(i==0){
+				first = tmp ;
+			}else{
+				
+				if(nums[i+1] >= nums[i]){
+					second = first ;
+					first = nums[i+1] ;
+				}else{
+					second = tmp ;
+				}
+				
+			}
+		}
+		
+		
+		
+		return second ;
+		
+		
+		
 	}
 
 }
