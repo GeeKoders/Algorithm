727f9a47af69ed93466b2e5df461825489408471
GeeKoders <iloveitone@gmail.com>
2021-02-11 11:14:32 +0800

    20210211 add Kth Largest Element in an Array with heap enhancement

--- src/Interview/Google/Array/KthLargestElementinanArray.java
+++ src/Interview/Google/Array/KthLargestElementinanArray.java
@@ -1,6 +1,7 @@
 package Interview.Google.Array;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class KthLargestElementinanArray {
 
@@ -29,4 +30,64 @@ public class KthLargestElementinanArray {
 		return nums[n - k];
 
 	}
+	/*
+	 * 
+	 * Your runtime beats 31.72 % of java submissions.
+	 * Your memory usage beats 99.39 % of java submissions.
+	 * 
+	 * Time complexity: O(NlogN) 
+	 * Space complexity: O(N)
+	 * 
+	 */
+	public int findKthLargest2(int[] nums, int k) {
+
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>() ;
+        
+        for(int i=0; i<nums.length; i++){
+            heap.add(nums[i]) ;
+        }
+        
+        int n = nums.length ;
+        int count = 0 ;
+        int res = 0 ;
+        
+        while(count != (n - k + 1)){
+            res = heap.remove() ;
+            count++ ;
+        }
+        
+        return res ;
+
+	}
+	/*
+	 * 
+	 * Your runtime beats 60.30 % of java submissions.
+	 * Your memory usage beats 78.99 % of java submissions.
+	 * 
+	 * Time complexity: O(Nlogk) 
+	 * Space complexity: O(k)
+	 * 
+	 * 
+	 */
+	public int findKthLargest3(int[] nums, int k) {
+
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>() ;
+        
+        for(int i=0; i<nums.length; i++){
+            heap.add(nums[i]) ;
+            
+            if(heap.size()>k){
+            	heap.poll();
+            }
+            
+        }
+        
+       
+        
+        return heap.poll() ;
+
+	}
+	
 }
