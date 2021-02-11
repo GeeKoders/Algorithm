0d9555bf2cef5a6b81ebf078c505d22fcec7d08c
GeeKoders <iloveitone@gmail.com>
2021-02-11 17:51:36 +0800

    20210211 add Backspace String Compare solution2

--- src/Interview/Google/Array/BackspaceStringCompare.java
+++ src/Interview/Google/Array/BackspaceStringCompare.java
@@ -47,5 +47,40 @@ public class BackspaceStringCompare {
 		return String.valueOf(stack);
 
 	}
+	
+	/*
+	 * Runtime: 0 ms, faster than 100.00% of Java online submissions for Backspace String Compare.
+	 * Memory Usage: 37.5 MB, less than 34.22% of Java online submissions for Backspace String Compare.
+	 * 
+	 * Time complexity: O(M+N)
+	 * Space complexity: O(1)
+	 * 
+	 */
+	public boolean backspaceCompare2(String S, String T) {
+        
+	     int i = S.length() - 1, j = T.length() - 1;
+	        int skipS = 0, skipT = 0;
+
+	        while (i >= 0 || j >= 0) { // While there may be chars in build(S) or build (T)
+	            while (i >= 0) { // Find position of next possible char in build(S)
+	                if (S.charAt(i) == '#') {skipS++; i--;}
+	                else if (skipS > 0) {skipS--; i--;}
+	                else break;
+	            }
+	            while (j >= 0) { // Find position of next possible char in build(T)
+	                if (T.charAt(j) == '#') {skipT++; j--;}
+	                else if (skipT > 0) {skipT--; j--;}
+	                else break;
+	            }
+	            // If two actual characters are different
+	            if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j))
+	                return false;
+	            // If expecting to compare char vs nothing
+	            if ((i >= 0) != (j >= 0))
+	                return false;
+	            i--; j--;
+	        }
+	        return true;
+	}
 
 }
