edea5c31201c4f104cdd6ee14efd0959277e918c
GeeKoders <iloveitone@gmail.com>
2021-02-12 20:24:47 +0800

    20210212 add Find the Celebrity with logical deduction

--- src/Tag/Array/FindtheCelebrity.java
+++ src/Tag/Array/FindtheCelebrity.java
@@ -15,6 +15,10 @@ public class FindtheCelebrity {
 	 * 
 	 * Brute Force
 	 * 
+	 * Runtime: 22 ms, faster than 47.40% of Java online submissions for Find the Celebrity.
+	 * Memory Usage: 39.1 MB, less than 85.50% of Java online submissions for Find the Celebrity.
+	 * 
+	 * 
 	 * Time complexity: O(N^2)
 	 * Space complexity: O(1)
 	 * 
@@ -43,6 +47,33 @@ public class FindtheCelebrity {
 		}
 
 		return true;
+
+	}
+	/*
+	 * Logical deduction
+	 * 
+	 * Runtime: 19 ms, faster than 79.11% of Java online submissions for Find the Celebrity.
+	 * Memory Usage: 39.7 MB, less than 27.03% of Java online submissions for Find the Celebrity.
+	 * 
+	 * Time complexity: O(N)
+	 * Space complexity: O(1)
+	 * 
+	 */
+	public int findCelebrity2(int n) {
+
+		int possible_celeb = 0 ;
+		
+		for (int i = 0; i < n; i++) {
+			if (knows(i, possible_celeb)) {
+				possible_celeb = i ;
+			}
+		}
+		
+		if(isCeleb(possible_celeb, n)){
+			return possible_celeb ;
+		}
+
+		return -1;
 
 	}
 }
