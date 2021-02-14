1f72f73ab5649e969db137f3091edf424529137d
GeeKoders <iloveitone@gmail.com>
2021-02-14 18:20:17 +0800

    20210214 split test

--- src/Test.java
+++ src/Test.java
@@ -17,9 +17,11 @@ public class Test {
 //	    System.out.println("key:" + key);
 //	    System.out.println("value:" + value);
 		
-		int[] nums = {5,6,7,8,9,10} ;
+//		int[] nums = {5,6,7,8,9,10} ;
+//		
+//		System.out.println(test.findKLarget(nums));
 		
-		System.out.println(test.findKLarget(nums));
+		test.splitTest();
 		
 	}
 	
@@ -80,6 +82,23 @@ public class Test {
 		
 		
 		
+	}
+	
+	private void splitTest(){
+		
+		String s = " A B   C D E" ;
+		StringBuilder sb = new StringBuilder() ;
+		String[] arr = s.split(" ") ;
+		for(int i=0; i<arr.length; i++){
+			System.out.println("s++" + arr[i] + "--b");
+			
+//			if(!arr[i].equals("")){
+				sb.append(arr[i]).append(" ") ;
+//			}
+			
+		}
+		System.out.println("length:" + arr.length);
+		System.out.println(sb.substring(0, sb.length() - 1));
 	}
 
 }
