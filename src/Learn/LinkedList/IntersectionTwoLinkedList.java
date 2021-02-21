e577130aedba85e2e9ad2690b03b0ca9cb6219e5
GeeKoders <iloveitone@gmail.com>
2021-02-21 16:31:23 +0800

    20210221 practice Intersection of Two Linked Lists

--- src/Learn/LinkedList/IntersectionTwoLinkedList.java
+++ src/Learn/LinkedList/IntersectionTwoLinkedList.java
@@ -1,17 +1,32 @@
-package LinkedList;
+package Learn.LinkedList;
 
 public class IntersectionTwoLinkedList {
 
-	
-	/*
-	 * https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1215/
-	 * 
-	 */
-	
-	public static void main(String[] args) {
 
+	class ListNode{
+		
+		int val ;
+		ListNode next ;
+		public ListNode(int val){
+			this.val = val ;
+		}
 	}
 	
+	
+	/*
+	 * 160. Intersection of Two Linked Lists (Easy)
+	 * 
+	 * https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1215/
+	 * 
+	 * solution: https://leetcode.com/problems/intersection-of-two-linked-lists/solution/
+	 * 
+	 * Runtime: 1 ms, faster than 97.80% of Java online submissions for Intersection of Two Linked Lists.
+	 * Memory Usage: 42 MB, less than 51.67% of Java online submissions for Intersection of Two Linked Lists.
+	 * 
+	 * Time complexity: O(M+N)
+	 * Space complexity: O(1)
+	 * 
+	 */
 	 public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 	        if(headA == null || headB == null){
 	            return null;
