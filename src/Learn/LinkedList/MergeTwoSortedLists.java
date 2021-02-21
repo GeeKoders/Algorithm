f370918c56d973e1d07e70171ed0d1be33e4f219
GeeKoders <iloveitone@gmail.com>
2021-02-21 18:18:59 +0800

    20210221 practice Merge Two Sorted Lists

--- src/Learn/LinkedList/MergeTwoSortedLists.java
+++ src/Learn/LinkedList/MergeTwoSortedLists.java
@@ -99,5 +99,40 @@ public class MergeTwoSortedLists {
         
         return dummy.next ;
     }
+	
+	public ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+		
+		if(l1 == null && l2 == null) return null ;
+        
+        ListNode dummy = new ListNode(-1) ;
+        ListNode curr = dummy ;
+        
+        while(l1 != null && l2 !=null){
+            
+                if(l1.val <= l2.val){
+                    curr.next = new ListNode(l1.val)  ;
+                    l1 = l1.next ;
+                }else{
+                    curr.next = new ListNode(l2.val)  ;
+                    l2 = l2.next ;
+                }
+                
+                curr = curr.next ;
+        }
+        
+         while(l1 != null){
+                curr.next = new ListNode(l1.val)  ;
+                l1 = l1.next ;
+                curr = curr.next ;
+        }
+                
+        while(l2 != null){
+            curr.next = new ListNode(l2.val)  ;
+            l2 = l2.next ;
+            curr = curr.next ;
+        }
+        
+        return dummy.next ;
+	}
 
 }
