f64886258086bc4447ef73373f976e448111e0db
GeeKoders <iloveitone@gmail.com>
2021-02-21 11:09:47 +0800

    20210221 practice insert LinkedList

---
+++ src/Tag/LinkedList/LinkedList.java
@@ -0,0 +1,111 @@
+package Tag.LinkedList;
+
+public class LinkedList {
+
+	
+	
+	public static void main(String[] args) {
+		
+		LinkedList linkedList = new LinkedList() ;
+		
+//		linkedList.insertHead(1);
+//		linkedList.insertHead(2);
+//		linkedList.insertHead(3);
+		linkedList.insertTail(1);
+		linkedList.insertTail(2);
+		linkedList.insertTail(3);
+		linkedList.insertMiddle(4, 2);
+		linkedList.insertMiddle(5, 4);
+		linkedList.traversal();
+	}
+	
+	class Node{
+		
+		int val ;
+		Node next ;
+		public Node(int val){
+			this.val = val ;
+		}
+		
+	}
+	
+	private Node head ;
+	
+	public void insertHead(int data){
+		
+		Node newNode = new Node(data) ;
+		
+		if(head == null){
+			
+			head = newNode ;
+			
+		}else{
+			newNode.next = head ;
+			head = newNode ;
+		}
+		
+	}
+	
+	public void insertMiddle(int data, int after){
+		
+		Node newNode = new Node(data) ;
+		
+		Node cur = head ;
+		
+		if(head == null){
+			head = newNode ;
+		}else{
+
+			while(cur!=null && cur.next!=null){
+				
+				
+				if(cur.next.val == after){
+					newNode.next = cur.next.next ;
+					cur.next.next = newNode ;
+				}
+				
+				cur = cur.next;
+				
+			}
+			
+		}
+		
+	}
+	
+	public void insertTail(int data){
+		
+		Node newNode = new Node(data) ;
+		
+		if(head == null){
+			head = newNode ;
+		}else{
+			
+			Node cur = head ;
+			Node prev = null ;
+			while(cur != null){
+				prev = cur ;
+				cur = cur.next ;
+			}
+			
+			prev.next = newNode ;
+			
+		}
+		
+	}
+	
+	public void traversal(){
+		
+		Node cur = head ;
+		
+		while(cur != null){
+			System.out.print(cur.val + " ");
+			cur = cur.next ;
+		}
+	}
+	
+	
+	
+	
+	
+	
+}
