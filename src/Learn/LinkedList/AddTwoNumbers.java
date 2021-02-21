0a5a3a87a7dacd7a80a9705b1ca5d5b3a519ef2c
GeeKoders <iloveitone@gmail.com>
2021-02-21 16:13:30 +0800

    20210221 practice Add Two Numbers

--- src/Learn/LinkedList/AddTwoNumbers.java
+++ src/Learn/LinkedList/AddTwoNumbers.java
@@ -70,5 +70,40 @@ public class AddTwoNumbers {
         }
         return dummyHead.next ;
     }
+	/*
+	 * Runtime: 2 ms, faster than 77.52% of Java online submissions for Add Two Numbers.
+	 * Memory Usage: 39.6 MB, less than 38.83% of Java online submissions for Add Two Numbers.
+	 * 
+	 */
+	public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+		
+		 ListNode dummy = new ListNode(0) ;
+	       ListNode cur = dummy ;
+	       int carry = 0 ;
+	        
+	       while(l1 != null || l2 != null){
+	           
+	           int x = (l1 == null) ? 0 : l1.val  ;
+	           int y = (l2 == null) ? 0 : l2.val ;
+	           
+	           int sum = x + y + carry ;
+	           carry = sum / 10 ; 
+	           cur.next = new ListNode(sum % 10) ;
+	           cur = cur.next ;
+	           if(l1 != null) l1 =l1.next ;
+	           if(l2 != null) l2 = l2.next ;
+	       } 
+	        
+	       if(carry > 0){
+	           cur.next = new ListNode(carry) ;
+	       } 
+	        
+	        
+	       return dummy.next ; 
+	        
+		
+		
+	}
+	
 
 }
