26695ab18300fb768af834857a4263c8a6a1de9b
GeeKoders <iloveitone@gmail.com>
2021-02-22 08:24:14 +0800

    20210222 practice Binary Tree Preorder Traversal

--- src/Learn/BinaryTree/BinaryTreePreorderTraversal.java
+++ src/Learn/BinaryTree/BinaryTreePreorderTraversal.java
@@ -1,4 +1,4 @@
-package BinaryTree;
+package Learn.BinaryTree;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,7 +7,11 @@ public class BinaryTreePreorderTraversal {
 
 	
 	/*
+	 * 144. Binary Tree Preorder Traversal (Medium)
+	 * 
 	 * https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/928/
+	 * 
+	 * solution: https://leetcode.com/problems/binary-tree-preorder-traversal/solution/
 	 * 
 	 * Given a binary tree, return the preorder traversal of its nodes' values.
 	 * 
@@ -22,8 +26,18 @@ public class BinaryTreePreorderTraversal {
 	 * Follow up: Recursive solution is trivial, could you do it iteratively?
 	 *    
 	 */
-	public static void main(String[] args) {
-
+	
+	class TreeNode{
+		
+		int val ;
+		TreeNode left ;
+		TreeNode right ;
+		public TreeNode(int val, TreeNode left, TreeNode right){
+			this.val = val ;
+			this.left = left ;
+			this.right = right ;
+		}
+		
 	}
 	
 	private List<Integer> res ; 
@@ -42,5 +56,34 @@ public class BinaryTreePreorderTraversal {
         inOrderTraversal(current.left) ;
         inOrderTraversal(current.right) ;
     }
+    /*
+     * 
+     * 
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Tree Preorder Traversal.
+     * Memory Usage: 37.2 MB, less than 65.93% of Java online submissions for Binary Tree Preorder Traversal.
+     * 
+     * Time complexity: O(logN)
+     * Space complexity: O(N)
+     * 
+     */
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        
+        List<Integer> res = new ArrayList<>() ;
+        
+        return traversal(res, root) ;
+        
+    }
+    
+    private List<Integer> traversal(List<Integer> res, TreeNode root){
+        
+        if(root == null) return res ;
+        
+        res.add(root.val) ;
+        
+        traversal(res, root.left) ;
+        traversal(res, root.right) ;
+        
+        return res ;
+    } 
 
 }
