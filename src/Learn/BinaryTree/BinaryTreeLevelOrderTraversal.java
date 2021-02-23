dce03ac6af31b56fe7d08c018a72f2d5895540e9
GeeKoders <iloveitone@gmail.com>
2021-02-24 06:53:14 +0800

    20210224 practice Binary Tree Level Order

--- src/Learn/BinaryTree/BinaryTreeLevelOrderTraversal.java
+++ src/Learn/BinaryTree/BinaryTreeLevelOrderTraversal.java
@@ -1,4 +1,4 @@
-package BinaryTree;
+package Learn.BinaryTree;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -8,7 +8,11 @@ import java.util.Queue;
 public class BinaryTreeLevelOrderTraversal {
 
 	/*
+	 * 102. Binary Tree Level Order (Medium)
+	 * 
 	 * https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/931
+	 * 
+	 * solution: https://leetcode.com/problems/binary-tree-level-order-traversal/solution/
 	 * 
 	 * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
 	 * For example:
@@ -25,9 +29,25 @@ public class BinaryTreeLevelOrderTraversal {
   		[9,20],
   		[15,7]
 	   ]
+	   
+	   Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Tree Level Order Traversal.
+	   Memory Usage: 39.3 MB, less than 44.43% of Java online submissions for Binary Tree Level Order Traversal.
+	   
+	   Time complexity: O(N)
+	   Space complexity: O(N)
+	   
 	 */
-	public static void main(String[] args) {
 
+	class TreeNode{
+		
+		int val ;
+		TreeNode left ;
+		TreeNode right ;
+		
+		public TreeNode(int val){
+			this.val = val ;
+		}
+		
 	}
 	
 	private List<Integer> list ;
