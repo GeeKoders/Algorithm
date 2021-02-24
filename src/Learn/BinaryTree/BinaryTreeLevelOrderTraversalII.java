bdcf1eeea5c4c0c37ecc96ff2908ea68e0e9e06d
GeeKoders <iloveitone@gmail.com>
2021-02-24 08:09:30 +0800

    20210224 add Binary Tree Level Order Traversal II with recursive

--- src/Learn/BinaryTree/BinaryTreeLevelOrderTraversalII.java
+++ src/Learn/BinaryTree/BinaryTreeLevelOrderTraversalII.java
@@ -71,4 +71,41 @@ public class BinaryTreeLevelOrderTraversalII {
 		return res;
 
 	}
+	
+	/*
+	 * Runtime: 19 ms, faster than 8.22% of Java online submissions for Binary Tree Level Order Traversal II.
+	 * Memory Usage: 40.2 MB, less than 9.97% of Java online submissions for Binary Tree Level Order Traversal II.
+	 * 
+	 * Time complexity: O(N)
+	 * Space complexity: O(N)	
+	 * 
+	 */
+	private List<List<Integer>> res2 = new ArrayList<>() ;
+    
+    public List<List<Integer>> levelOrderBottom2(TreeNode root) {
+        
+      
+        if(root == null) return res2 ;
+        
+        helper(root, 0) ;
+        
+        Collections.reverse(res2) ;
+        
+        return res2 ;
+        
+    }
+    
+    private void helper(TreeNode root, int level){
+        System.out.println("level:" + level) ;
+        System.out.println("size():" + res.size()) ;
+        if(res2.size() == level){
+            res2.add(new ArrayList<>()) ;
+        }
+        
+        res2.get(level).add(root.val) ;
+        
+        if(root.left != null) helper(root.left, level + 1) ;
+        if(root.right != null) helper(root.right, level + 1) ;
+        
+    }
 }
