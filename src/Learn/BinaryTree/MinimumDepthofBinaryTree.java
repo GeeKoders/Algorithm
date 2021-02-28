613b3ff24f150458a87777e16508fefa790410cb
GeeKoders <iloveitone@gmail.com>
2021-02-28 13:23:30 +0800

    20210228 add Minimum Depth of Binary Tree

---
+++ src/Learn/BinaryTree/MinimumDepthofBinaryTree.java
@@ -0,0 +1,51 @@
+package Learn.BinaryTree;
+
+public class MinimumDepthofBinaryTree {
+
+	class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		public TreeNode(int val) {
+			this.val = val;
+		}
+
+	}
+	/*
+	 * Minimum Depth of Binary Tree (Easy)
+	 * 
+	 * https://leetcode.com/problems/minimum-depth-of-binary-tree/
+	 * 
+	 * https://leetcode.com/problems/minimum-depth-of-binary-tree/solution/
+	 * 
+	 * Runtime: 4 ms, faster than 72.35% of Java online submissions for Minimum Depth of Binary Tree.
+	 * Memory Usage: 60.1 MB, less than 57.08% of Java online submissions for Minimum Depth of Binary Tree.
+	 * 
+	 * Time complexity: O(N)
+	 * Space complexity: O(N)
+	 * 
+	 */
+	public int minDepth(TreeNode root) {
+
+		if (root == null)
+			return 0;
+		if (root.left == null && root.right == null) {
+			return 1;
+		}
+
+		int res = Integer.MAX_VALUE;
+
+		if (root.left != null) {
+			res = Math.min(res, minDepth(root.left));
+		}
+
+		if (root.right != null) {
+			res = Math.min(res, minDepth(root.right));
+		}
+
+		return res + 1;
+
+	}
+
+}
