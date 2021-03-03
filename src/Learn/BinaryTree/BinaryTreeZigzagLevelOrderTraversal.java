4ce454f0d82e37755e28d0ab2d42c96c68e18f7b
GeeKoders <iloveitone@gmail.com>
2021-03-04 06:26:00 +0800

    20210304 add Binary Tree Zigzag Level Order Traversal with bfs

--- src/Learn/BinaryTree/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/Learn/BinaryTree/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,7 +1,9 @@
 package Learn.BinaryTree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BinaryTreeZigzagLevelOrderTraversal {
 
@@ -60,4 +62,39 @@ public class BinaryTreeZigzagLevelOrderTraversal {
 			dfs(root.right, level + 1, results);
 
 	}
+	
+	public List<List<Integer>> zigzagLevelOrder2(TreeNode root) {
+        List<List<Integer>> list = new LinkedList<>();
+        if(root == null){
+            return list;
+        }
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        boolean even = true;
+        
+        while(!q.isEmpty()){
+            int n = q.size();
+            LinkedList<Integer> subList = new LinkedList<>();
+            
+            for(int i = 0; i < n; i++){
+                TreeNode top = q.poll();
+                
+                if(top.left != null){
+                    q.add(top.left);
+                }
+                if(top.right != null){
+                    q.add(top.right);
+                }
+                if(even)
+                    subList.add(top.val);
+                else
+                    subList.addFirst(top.val);
+            }
+            list.add(subList);
+            even = !even;
+        }
+        
+        return list;
+    }
 }
