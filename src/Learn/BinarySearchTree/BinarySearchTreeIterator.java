49524b0f547c5cc23e6fcddffa35d9e06489a4f3
GeeKoders <iloveitone@gmail.com>
2021-03-09 21:30:36 +0800

    20210309 add Binary Search Tree Iterator with stack

--- src/Learn/BinarySearchTree/BinarySearchTreeIterator.java
+++ src/Learn/BinarySearchTree/BinarySearchTreeIterator.java
@@ -2,6 +2,7 @@ package BinarySearchTree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinarySearchTreeIterator {
 
@@ -25,9 +26,14 @@ public class BinarySearchTreeIterator {
 	}
 	
 	/*
+	 * 173. Binary Search Tree Iterator (Medium)
+	 * 
 	 * https://leetcode.com/explore/learn/card/introduction-to-data-structure-binary-search-tree/140/introduction-to-a-bst/1008/
 	 * 
 	 * solution: https://leetcode.com/problems/binary-search-tree-iterator/solution/
+	 *
+	 * Runtime: 14 ms, faster than 99.38% of Java online submissions for Binary Search Tree Iterator.
+	 * Memory Usage: 42.6 MB, less than 75.65% of Java online submissions for Binary Search Tree Iterator.
 	 *
 	 */
 	
@@ -69,6 +75,56 @@ public class BinarySearchTreeIterator {
     // O(1)
     public boolean hasNext() {
         return index+1 < list.size() ;
+    }
+    
+    /*
+     * Time complexity: refer to the solution
+     * Space complexity: O(N)
+     * 
+     */
+    Stack<TreeNode> stack;
+
+    public void BSTIterator2(TreeNode root) {
+
+        // Stack for the recursion simulation
+        this.stack = new Stack<TreeNode>();
+
+        // Remember that the algorithm starts with a call to the helper function
+        // with the root node as the input
+        this._leftmostInorder(root);
+    }
+
+    private void _leftmostInorder(TreeNode root) {
+
+        // For a given node, add all the elements in the leftmost branch of the tree
+        // under it to the stack.
+        while (root != null) {
+            this.stack.push(root);
+            root = root.left;
+        }
+    }
+
+    /**
+     * @return the next smallest number
+     */
+    public int next2() {
+        // Node at the top of the stack is the next smallest element
+        TreeNode topmostNode = this.stack.pop();
+
+        // Need to maintain the invariant. If the node has a right child, call the
+        // helper function for the right child
+        if (topmostNode.right != null) {
+            this._leftmostInorder(topmostNode.right);
+        }
+
+        return topmostNode.val;
+    }
+
+    /**
+     * @return whether we have a next smallest number
+     */
+    public boolean hasNext2() {
+        return this.stack.size() > 0;
     }
 
 }
