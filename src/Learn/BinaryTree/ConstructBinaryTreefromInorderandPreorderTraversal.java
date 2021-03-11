e27ade918f42dc4aeeb3be85e310be97ddc07c13
GeeKoders <iloveitone@gmail.com>
2021-03-12 07:10:29 +0800

    20210312 add Construct Binary Tree from Preorder and Inorder Traversal with hashmap

--- src/Learn/BinaryTree/ConstructBinaryTreefromInorderandPreorderTraversal.java
+++ src/Learn/BinaryTree/ConstructBinaryTreefromInorderandPreorderTraversal.java
@@ -1,5 +1,8 @@
 package Learn.BinaryTree;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConstructBinaryTreefromInorderandPreorderTraversal {
 
 	class TreeNode{
@@ -84,6 +87,30 @@ public class ConstructBinaryTreefromInorderandPreorderTraversal {
 		
 		return root ;
 		
+	}
+	
+	public TreeNode buildTree2(int[] preorder, int[] inorder) {
+	    Map<Integer, Integer> inMap = new HashMap<Integer, Integer>();
+
+	    for(int i = 0; i < inorder.length; i++) {
+	        inMap.put(inorder[i], i);
+	    }
+
+	    TreeNode root = buildTree2(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, inMap);
+	    return root;
+	}
+
+	public TreeNode buildTree2(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd, Map<Integer, Integer> inMap) {
+	    if(preStart > preEnd || inStart > inEnd) return null;
+
+	    TreeNode root = new TreeNode(preorder[preStart]);
+	    int inRoot = inMap.get(root.val);
+	    int numsLeft = inRoot - inStart;
+
+	    root.left = buildTree2(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1, inMap);
+	    root.right = buildTree2(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd, inMap);
+
+	    return root;
 	}
 	
 
