c2eb1568692377354ba1757586fc5eddb3c2784b
GeeKoders <iloveitone@gmail.com>
2021-03-19 08:02:27 +0800

    20210319 add Simplify Path reference

--- src/Tag/Stack/SimplifyPath.java
+++ src/Tag/Stack/SimplifyPath.java
@@ -11,6 +11,8 @@ public class SimplifyPath {
 	 * 
 	 * solution: https://leetcode.com/problems/simplify-path/solution/
 	 * 
+	 * reference: https://www.youtube.com/watch?v=SxuGQnlsXcw&ab_channel=JacobHuang
+	 * 
 	 * Runtime: 8 ms, faster than 29.80% of Java online submissions for Simplify Path.
 	 * Memory Usage: 39.2 MB, less than 44.01% of Java online submissions for Simplify Path.
 	 * 
